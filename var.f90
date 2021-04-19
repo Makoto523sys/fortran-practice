99f8fbd64798ce846f511b8ea9a008ea640bf026
Makoto <ookunimasaharu@gmail.com>
2021-04-19 12:46:14 +0900

    変数の実験

---
+++ var.f90
@@ -0,0 +1,13 @@
+program main
+    integer :: a = 99
+    real :: b = 5.2
+    complex :: c = (3.0, 1.0)
+    real, parameter :: pi = 3.14
+    character(3) d
+    character(len=4) :: e = "ABCD"
+    d = "xyz"
+    print *, a, b , c
+    print *, c * pi
+    print *, e
+    print *, d
+end program main
