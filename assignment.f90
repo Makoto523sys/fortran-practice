1c21eeffd598d32ea9f319d0d148a6bd2b010943
Makoto <ookunimasaharu@gmail.com>
2021-04-19 13:12:02 +0900

    代入やってみた

---
+++ assignment.f90
@@ -0,0 +1,12 @@
+program assignment
+    implicit none
+    integer a
+    real b
+    double precision c
+    character(len=3) d
+    a = 55
+    b = 1.23
+    c = 1.23d5
+    d = "ABCD"
+    print *, a, b, c, d
+end program assignment
